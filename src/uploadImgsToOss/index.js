02c52cf61707fa5a010cb5c35d18f45348513049
17620387800@bokecad.com <17620387800@bokecad.com>
2019-06-03 11:37:19 +0800

    完成图片上传

--- src/uploadImgsToOss/index.js
+++ src/uploadImgsToOss/index.js
@@ -7,7 +7,9 @@ export default class UploadImgsToOss extends Component {
     maxNum: PropTypes.number,
     imageUrls: PropTypes.array,
     getOss: PropTypes.func,
-    successUpload: PropTypes.func
+    successUpload: PropTypes.func,
+    definedImage: PropTypes.string,
+    defineChange: PropTypes.func
   }
   constructor(props) {
     super(props)
@@ -16,7 +18,8 @@ export default class UploadImgsToOss extends Component {
       maxNum: this.props.maxNum || 6,
       showDelete: -1,
       drapImage: '',
-      drapIndex: -1
+      drapIndex: -1,
+      definedImage: this.props.definedImage
     }
   }
   // 选择图片
@@ -73,41 +76,51 @@ export default class UploadImgsToOss extends Component {
     // 删除
     this.props.successUpload(img)
   }
-  onDragStart = (e, image, index) => {
+  onDrop = (e, image, index) => {
+    e.preventDefault()
+    let {drapIndex, drapImage, imageUrls} = this.state
+    if (drapImage === image) return
+    imageUrls.splice(drapIndex, 1, ...imageUrls.splice(index, 1, imageUrls[drapIndex]))
+    this.setState({
+      imageUrls
+    })
+  }
+  onDragLeave = (e, image, index) => {
     this.setState({
       drapImage: image,
       drapIndex: index
     })
   }
-  onDrop = (e, image, index) => {
-    let {drapIndex, drapImage, imageUrls} = this.state
-    if(drapImage === image) return ;
-    imageUrls.splice(drapIndex, 1, ...imageUrls.splice(index, 1, imageUrls[drapIndex]));
+  // 点击设置默认
+  clickDefined = (e, image, index) => {
+    e.preventDefault()
     this.setState({
-      imageUrls
+      definedImage: image
     })
-  }
-  onDragLeave = (e, image) => {
-
+    this.props.defineChange(image, index)
   }
   render() {
-    const { imageUrls, maxNum, showDelete } = this.state
+    const { imageUrls, maxNum, showDelete, definedImage } = this.state
     return (
       <div className={styles['boke-c-uploadImgs']} style={{width: 100 + 110 * imageUrls.length + 'px'}} >
         {imageUrls.map((item, index) => (
           <div
             draggable='true'
-            droppable='true'
-            onDragOver={(e) => this.onDrop(e, item, index)}
+            onDragOverCapture={(e) => this.onDrop(e, item, index)}
             onDragLeave={(e) => this.onDragLeave(e, item, index)}
-            onDragStart={(e) => this.onDragStart(e, item, index)}
             className={styles['boke-upload-images']}
             onMouseLeave={() => { this.setState({showDelete: -1}) }}
             onMouseEnter={() => { this.setState({showDelete: index}) }}
             key={index}>
             <img className={styles['upload-img']} style={{width: '98px'}} alt='' src={item} />
             {
-              showDelete === index ? <div className={styles['delete-this-imge']}><img onClick={() => { this.delete(item) }} alt='删除' src={require('./delete.svg')} /></div> : ''
+              showDelete === index
+                ? <div className={styles['delete-this-imge']}><img onClick={() => { this.delete(item) }} alt='删除' src={require('./delete.svg')} /></div> : ''
+            }
+            {
+              definedImage
+                ? definedImage === item
+                  ? <div className={styles['defined-img']}>默认</div> : <button onClick={(e) => this.clickDefined(e, item, index)} className={styles['set-defined-img']}>设置默认</button> : ''
             }
           </div>
         ))}
